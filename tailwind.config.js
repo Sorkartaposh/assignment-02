76763b63fa20f797912a042d72b169d827c42c9b
sorkartaposh <taposhsorkar88@gmail.com>
2025-07-25 20:48:40 +0600

    our plants section added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // inter: ["Inter", "sans-serif"],
+        
+      },
+      // colors: {
+      //   primary: "",
+      //   secondary: "",
+      // },
+    },
+    
+  },
+  plugins: [require("daisyui")],
+}
